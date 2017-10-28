d5f0008e218916bd50c157be20c14ce9b8534d3b
Steven <>
2017-10-28 10:37:15 +0200

    Remove test dependencies

--- app/src/main/java/com/steven/muzeillect/SettingsActivity.kt
+++ app/src/main/java/com/steven/muzeillect/SettingsActivity.kt
@@ -2,14 +2,14 @@ package com.steven.muzeillect
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.ImageButton
+import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_settings)
-		findViewById<ImageButton>(R.id.back).setOnClickListener {
+		back.setOnClickListener {
 			finish()
 		}
 	}
