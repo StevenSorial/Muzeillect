b8ad3574a98180f05568cabebc691f41e28c9cf7
Steven <>
2017-12-20 13:25:26 +0200

    Improve readability

--- app/src/main/java/com/steven/muzeillect/SettingsActivity.kt
+++ app/src/main/java/com/steven/muzeillect/SettingsActivity.kt
@@ -2,7 +2,7 @@ package com.steven.muzeillect
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_settings.*
+import kotlinx.android.synthetic.main.activity_settings.back
 
 class SettingsActivity : AppCompatActivity() {
 
