99277b3510763878918e02e1f58104edf7210eb6
Steven <StevenMagdy92@gmail.com>
2019-02-12 13:30:59 +0200

    Fix Up Button behavior

--- app/src/main/java/com/steven/muzeillect/MainActivity.kt
+++ app/src/main/java/com/steven/muzeillect/MainActivity.kt
@@ -3,19 +3,33 @@ package com.steven.muzeillect
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+  private val appBarConfiguration by lazy {
+    AppBarConfiguration(if (isTaskRoot) setOf(R.id.fragment_settings_container) else emptySet())
+  }
+  private val navController by lazy { findNavController(R.id.navHostFragment) }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
     setSupportActionBar(toolbar)
-    toolbar.setupWithNavController(findNavController(R.id.navHostFragment))
+    setupActionBarWithNavController(navController, appBarConfiguration)
     supportActionBar?.elevation = 0f
     supportActionBar?.setDisplayShowTitleEnabled(false)
   }
 
-  override fun onSupportNavigateUp() = findNavController(R.id.navHostFragment).navigateUp()
+  override fun onSupportNavigateUp(): Boolean {
+    return if (navController.currentDestination?.id == R.id.fragment_settings_container) {
+      onBackPressed()
+      true
+    } else {
+      navController.navigateUp(appBarConfiguration)
+    }
+  }
 }
